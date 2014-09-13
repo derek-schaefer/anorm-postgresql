f9b85b186dfebe23a4b2589229d83ca81d5fd02d
Sean Smith <ssmith@gilt.com>
2014-09-13 15:37:51 -0400

    Add period support

--- src/main/scala/com/derekschaefer/anorm-postgresql/JodaTime.scala
+++ src/main/scala/com/derekschaefer/anorm-postgresql/JodaTime.scala
@@ -5,11 +5,14 @@ import anorm._
 import org.joda.time._
 import org.joda.time.format._
 
+import org.postgresql.util.PGInterval
+
 object JodaTime {
 
   val dateParser = ISODateTimeFormat.dateParser()
   val timeParser = ISODateTimeFormat.timeParser()
   val dateTimeParser = ISODateTimeFormat.dateTimeParser()
+  private val mathContext = new java.math.MathContext(4)
 
   implicit def rowToLocalDate: Column[LocalDate] = Column.nonNull { (value, meta) =>
     val MetaDataItem(qualified, nullable, clazz) = meta
@@ -68,6 +71,42 @@ object JodaTime {
   implicit def localDateTimeToStatement = new ToStatement[LocalDateTime] {
     def set(s: java.sql.PreparedStatement, index: Int, aValue: LocalDateTime) {
       s.setTimestamp(index, new java.sql.Timestamp(aValue.toDateTime.getMillis))
+    }
+  }
+
+  implicit val rowToPeriod: Column[Period] = Column.nonNull { (value, meta) =>
+    val MetaDataItem(qualified,nullable, clazz) = meta
+    value match {
+      case ts: org.postgresql.util.PGInterval => {
+        val years = ts.getYears
+        val months = ts.getMonths
+        val days = ts.getDays
+        val hours = ts.getHours
+        val mins = ts.getMinutes
+        val seconds = Math.floor(ts.getSeconds).asInstanceOf[Int]
+        val secondsAsBigDecimal = new java.math.BigDecimal(ts.getSeconds,mathContext)
+        val millis = secondsAsBigDecimal.subtract(new java.math.BigDecimal(seconds)).multiply(new java.math.BigDecimal(1000)).intValue
+
+        Right(new Period(years,months, 0, days, hours, mins, seconds,millis).normalizedStandard)
+      }
+      case _ => Left(TypeDoesNotMatch("Cannot convert " + value + ":" + value.asInstanceOf[AnyRef].getClass) )
+    }
+  }
+
+  implicit val periodToStatement = new ToStatement[Period] {
+    def set(s: java.sql.PreparedStatement, index: Int, aValue: Period): Unit = {
+      val years = aValue.getYears
+      val months = aValue.getMonths
+      val days = aValue.getWeeks*7 + aValue.getDays
+      val hours = aValue.getHours
+      val mins = aValue.getMinutes
+      val seconds = aValue.getSeconds
+      val millis = aValue.getMillis
+      val subseconds = new java.math.BigDecimal(millis,mathContext).divide(new java.math.BigDecimal(1000))
+      val combinedSecond = new java.math.BigDecimal(aValue.getSeconds,mathContext).add(subseconds).doubleValue
+
+      val interval = new PGInterval(years,months,days,hours,mins,combinedSecond)
+      s.setObject(index,interval)
     }
   }
 
